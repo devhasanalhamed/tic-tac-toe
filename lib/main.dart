4739d7cd373b893938b17edc71279e4de3dc7f47
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-21 23:39:50 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tic_tac_too/two_players_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,6 +34,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints(
+          maxWidth: 400.0,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const TwoPlayersScreen(),
+                  ),
+                ),
+                child: const Text("لعبة لـ لاعبين إثنين 🤺"),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("ضد الكمبيوتر 💻"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
