02daed49e9515313936200e2d3fadeb5e1003525
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-06-11 01:14:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:tic_tac_too/two_players_screen.dart';
-import 'package:tic_tac_too/versus_computer_screen.dart';
+import 'package:tic_tac_too/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,52 +24,7 @@ class MyApp extends StatelessWidget {
       ),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        constraints: const BoxConstraints(
-          maxWidth: 400.0,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ElevatedButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const TwoPlayersScreen(),
-                  ),
-                ),
-                child: const Text("لعبة لـ لاعبين إثنين 🤺"),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const VersusComputerScreen(),
-                  ),
-                ),
-                child: const Text("ضد الكمبيوتر 💻"),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
