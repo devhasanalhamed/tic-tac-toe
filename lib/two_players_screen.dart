6cdb656369424d43af789421da9d6e9b72519ec8
Hasan Al-Hamed <therealhassan97@gmail.com>
2024-06-18 00:11:49 +0300

    Update two_players_screen.dart

--- lib/two_players_screen.dart
+++ lib/two_players_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'core/function/show_result.dart';
 
 class TwoPlayersScreen extends StatefulWidget {
   const TwoPlayersScreen({super.key});
@@ -224,60 +225,5 @@ class _TwoPlayersScreenState extends State<TwoPlayersScreen> {
     clearBoard();
     playerOneScore = 0;
     playerTwoScore = 0;
-  }
-
-  Future<void> showResultDialog(bool isWin) async {
-    thereIsAWinner = true;
-    if (isWin) isPlayerOne ? playerOneScore += 1 : playerTwoScore += 1;
-    winnerName = isPlayerOne ? playerOne : playerTwo;
-    isPlayerOne = !isPlayerOne;
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return PopScope(
-          canPop: false,
-          child: Directionality(
-            textDirection: TextDirection.rtl,
-            child: AlertDialog(
-              actions: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          clearBoard();
-                          Navigator.pop(context);
-                        });
-                      },
-                      child: const Text("اللعب مجدداً"),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          Navigator.pop(context);
-                        });
-                      },
-                      child: const Text("عرض النتيجة"),
-                    ),
-                  ],
-                ),
-              ],
-              title: Text(
-                  isWin ? "مبرووووووووووووك 🥳" : "عوافي 🙂 نلعب مره ثانية"),
-              content: Container(
-                constraints: const BoxConstraints(maxWidth: 200),
-                child: Text(
-                  isWin
-                      ? "ألف مبروك يا $winnerName تستاهل الفوز، حاب نتحدى مرة ثانية؟"
-                      : "هذه المرة الفوز من نصيبي أتحداك تفوز 🔥",
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
   }
 }
