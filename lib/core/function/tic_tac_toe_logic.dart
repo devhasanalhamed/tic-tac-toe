e5a48d263c9b2c67b17375f9d5e08918b767c44a
Hasan Al-Hamed <81860602+devhasanalhamed@users.noreply.github.com>
2024-07-26 06:13:20 +0300

    Update tic_tac_toe_logic.dart
    I have converted the board from string to integer to easy calculate the best next move

--- lib/core/function/tic_tac_toe_logic.dart
+++ lib/core/function/tic_tac_toe_logic.dart
@@ -1,27 +1,21 @@
-bool? ticTacToeLogic(List<String> board, int roundCount) {
+bool? ticTacToeLogic(List<int> board, int roundCount) {
   print(board);
 
   for (int i = 0; i < board.length; i += 3) {
-    if (board[i].isNotEmpty &&
-        board[i] == board[i + 1] &&
-        board[i] == board[i + 2]) {
+    if (board[i] != 0 && board[i] == board[i + 1] && board[i] == board[i + 2]) {
       return true;
     }
   }
 
   for (int i = 0; i < 3; i++) {
-    if (board[i].isNotEmpty &&
-        board[i] == board[i + 3] &&
-        board[i] == board[i + 6]) {
+    if (board[i] != 0 && board[i] == board[i + 3] && board[i] == board[i + 6]) {
       return true;
     }
   }
 
-  if (board[0].isNotEmpty && board[0] == board[4] && board[0] == board[8]) {
+  if (board[0] != 0 && board[0] == board[4] && board[0] == board[8]) {
     return true;
-  } else if (board[2].isNotEmpty &&
-      board[2] == board[4] &&
-      board[2] == board[6]) {
+  } else if (board[2] != 0 && board[2] == board[4] && board[2] == board[6]) {
     return true;
   } else if (roundCount == 9) {
     return false;
